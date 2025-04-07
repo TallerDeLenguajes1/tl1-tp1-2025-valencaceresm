022b478d58d18086b72a6700bb770bac842ccdc8
valencaceresm <caceresmendoza.valentina@gmail.com>
2025-04-06 21:41:07 -0300

    Agrego puntero

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    printf("hola mundo\n");
+    int miVariable = 10;
+       int *miPuntero = &miVariable;
+
+       printf("Contenido del puntero: %p\n", miPuntero);
+       printf("Dirección de memoria almacenada por el puntero: %p\n", miPuntero);
+       printf("Dirección de memoria de la variable: %p\n", &miVariable);
+       printf("Dirección de memoria del puntero: %p\n", &miPuntero);
+       printf("Tamaño de memoria de la variable: %zu bytes\n", sizeof(miVariable));
+       
     return 0;
 }
